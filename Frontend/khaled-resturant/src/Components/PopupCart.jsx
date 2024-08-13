9275721433e29b6550409da6201e04b025ef20e9
Khaled <khaledonem89@gmail.com>
2024-08-13 17:06:11 +0300

    Toestfiy added

--- Frontend/khaled-resturant/src/Components/PopupCart.jsx
+++ Frontend/khaled-resturant/src/Components/PopupCart.jsx
@@ -1,8 +1,10 @@
-import { Box, Button, IconButton } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import { Add, CloseOutlined } from "@mui/icons-material";
 import { DecreaseQuantity, IncreaseQuantity } from "../Redux/CartSlice";
+import { toast } from "react-toastify";
+
 export default function PopupCart({ HandleCartCloser }) {
   // @ts-ignore
   const { SelectedProducts } = useSelector((state) => state.CartShop);
@@ -15,12 +17,20 @@ export default function PopupCart({ HandleCartCloser }) {
     return MyProductQ ? MyProductQ.quantity : 0;
   };
 
+  const IncreaseQuantityHandler = (item) => {
+    dispatch(IncreaseQuantity(item));
+    toast.success("Added to cart");
+  };
+
+  const DecreaseQuantityHandler = (item) => {
+    dispatch(IncreaseQuantity(item));
+    toast.success("Deleted from the cart");
+  };
 
   return (
     <Box className="relative w-full">
       <div
         className="absolute z-50  bg-white  rounded-lg shadow-2xl  max-sm:-translate-x-full  -translate-x-1/2 left-1/2  top-0 p-6 w-72 md:w-96 "
-
         aria-modal="true"
         role="dialog"
       >
@@ -32,7 +42,10 @@ export default function PopupCart({ HandleCartCloser }) {
         </IconButton>
 
         {SelectedProducts.map((item) => (
-          <div key={item.id} className="mt-4 space-y-6 relative   text-xs md:text-base">
+          <div
+            key={item.id}
+            className="mt-4 space-y-6 relative   text-xs md:text-base"
+          >
             <ul className="space-y-4 ">
               <li className="flex items-center gap-2">
                 <img
@@ -42,7 +55,9 @@ export default function PopupCart({ HandleCartCloser }) {
                 />
 
                 <div className="flex-1  ">
-                  <h3 className="text-md font-semibold text-gray-800">{item.name}</h3>
+                  <h3 className="text-md font-semibold text-gray-800">
+                    {item.name}
+                  </h3>
 
                   <dl className="mt-1 text-xs text-gray-500">
                     <div>
@@ -59,9 +74,10 @@ export default function PopupCart({ HandleCartCloser }) {
 
                 <div className="flex items-center justify-end gap-4">
                   <button
-                    onClick={() => dispatch(IncreaseQuantity(item))}
+                    onClick={() => {
+                      IncreaseQuantityHandler(item);
+                    }}
                     className="text-gray-600 hover:text-green-500 transition-all duration-200"
-                  
                   >
                     <Add />
                   </button>
@@ -69,10 +85,10 @@ export default function PopupCart({ HandleCartCloser }) {
                   <span>{ProductQ(item)}</span>
 
                   <button
-                    onClick={() => dispatch(DecreaseQuantity(item))}
+                    onClick={() => {
+                      DecreaseQuantityHandler(item);
+                    }}
                     className="text-gray-600 hover:text-red-500 transition-all duration-200"
-                  
-                    
                   >
                     <DeleteForeverIcon />
                   </button>
@@ -109,5 +125,4 @@ export default function PopupCart({ HandleCartCloser }) {
       </div>
     </Box>
   );
-
 }
